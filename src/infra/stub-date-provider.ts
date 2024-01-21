0892c4fa5c7c84da246948f0003d908748db424a
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-22 00:21:37 +0100

    feat(infra): add stub date provider

---
+++ src/infra/stub-date-provider.ts
@@ -0,0 +1,12 @@
+import type { DateProviderPort } from "@/application/ports/date-provider.port";
+
+export class StubDateProvider implements DateProviderPort {
+  now?: Date = undefined;
+
+  getNow(): Date {
+    if (this.now === undefined) {
+      throw new Error("Stub now date not set");
+    }
+    return this.now;
+  }
+}
