0a99b64c2493edc0d49c8526bd54e40612a9d3d0
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-21 23:05:32 +0100

    feat(domain): add user

---
+++ src/domain/user.ts
@@ -0,0 +1,83 @@
+import type { Email, ID, Password } from "@/types/super-types";
+
+type UserData = {
+  id: ID;
+  email: Email;
+  firstName: string;
+  lastName: string;
+  password: Password;
+  createdAt: Date;
+  updatedAt: Date | undefined;
+};
+
+export class User {
+  constructor(
+    private readonly _id: ID,
+    private readonly _email: Email,
+    private readonly _firstName: string,
+    private readonly _lastName: string,
+    private readonly _password: Password,
+    private readonly _createdAt: Date,
+    private readonly _updatedAt?: Date
+  ) {}
+
+  static fromData({
+    id,
+    email,
+    firstName,
+    lastName,
+    password,
+    createdAt,
+    updatedAt,
+  }: User["data"]): User {
+    return new User(
+      id,
+      email,
+      firstName,
+      lastName,
+      password,
+      createdAt,
+      updatedAt
+    );
+  }
+
+  get id(): ID {
+    return this._id;
+  }
+
+  get email(): Email {
+    return this._email;
+  }
+
+  get firstName(): string {
+    return this._firstName;
+  }
+
+  get lastName(): string {
+    return this._lastName;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  get createdAt(): Date {
+    return this._createdAt;
+  }
+
+  get updatedAt(): Date | undefined {
+    return this._updatedAt;
+  }
+
+  get data(): UserData {
+    return {
+      id: this.id,
+      email: this.email,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      password: this.password,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+    };
+  }
+}
