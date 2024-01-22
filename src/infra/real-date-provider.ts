f7a363aa4a93c98fbabdaef33f1398f580bf9ab0
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-22 01:52:04 +0100

    feat(infra): add real date provider

---
+++ src/infra/real-date-provider.ts
@@ -0,0 +1,7 @@
+import type { DateProviderPort } from "@/application/ports/date-provider.port";
+
+export class RealDateProvider implements DateProviderPort {
+  getNow(): Date {
+    return new Date();
+  }
+}
