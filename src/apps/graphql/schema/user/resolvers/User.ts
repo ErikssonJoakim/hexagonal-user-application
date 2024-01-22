063068ff131cf267a899858db142e3f7b60ca9a1
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-22 02:48:17 +0100

    feat(app): add user entity mapper

--- src/apps/graphql/schema/user/resolvers/User.ts
+++ src/apps/graphql/schema/user/resolvers/User.ts
@@ -1,9 +1,9 @@
-import type { UserResolvers } from "./../../types.generated";
+import type { UserResolvers } from "@/apps/graphql/schema/types.generated";
+
 export const User: UserResolvers = {
-  /* Implement User resolver logic here */
-    createdAt: () => { /* User.createdAt resolver is required because User.createdAt exists but UserMapper.createdAt does not */ },
-    firstName: () => { /* User.firstName resolver is required because User.firstName exists but UserMapper.firstName does not */ },
-    id: () => { /* User.id resolver is required because User.id exists but UserMapper.id does not */ },
-    lastName: () => { /* User.lastName resolver is required because User.lastName exists but UserMapper.lastName does not */ },
-    updatedAt: () => { /* User.updatedAt resolver is required because User.updatedAt exists but UserMapper.updatedAt does not */ }
+  createdAt: ({ created_at }) => created_at,
+  updatedAt: ({ updated_at }) => updated_at,
+  firstName: ({ first_name }) => first_name,
+  lastName: ({ last_name }) => last_name,
+  id: ({ user_id }) => user_id,
 };
