4f3d48ef899aaad21b1f10cd855c812fb4205b00
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-22 02:49:13 +0100

    feat(app): add create user resolver logic

--- src/apps/graphql/schema/user/resolvers/Mutation/createUser.ts
+++ src/apps/graphql/schema/user/resolvers/Mutation/createUser.ts
@@ -1,8 +1,27 @@
-import type { MutationResolvers } from "./../../../types.generated";
+import type { Context } from "@/apps/graphql/index";
+import type { MutationResolvers } from "@/apps/graphql/schema/types.generated";
+
 export const createUser: NonNullable<MutationResolvers["createUser"]> = async (
   _parent,
   _arg,
-  _ctx
+  _ctx: Context
 ) => {
-  /* Implement Mutation.createUser resolver logic here */
+  const { register, getByEmail } = _ctx.dataSources.userAPI;
+  await register(_arg.input);
+  const user = await getByEmail(_arg.input.email);
+
+  if (user) {
+    const { id, email, firstName, lastName, password, createdAt, updatedAt } =
+      user;
+
+    return {
+      user_id: id,
+      email,
+      first_name: firstName,
+      last_name: lastName,
+      password,
+      created_at: createdAt,
+      updated_at: updatedAt,
+    };
+  }
 };
