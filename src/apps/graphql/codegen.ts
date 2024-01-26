eb3385d17932c83495825e81a9adcc926a82aba3
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-26 21:25:53 +0100

    chore(app): move codegen file into graphql folder

--- src/apps/graphql/codegen.ts
+++ src/apps/graphql/codegen.ts
@@ -1,8 +1,9 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 import { defineConfig } from "@eddeee888/gcg-typescript-resolver-files";
+import path from "path";
 
 const config: CodegenConfig = {
-  schema: "src/apps/graphql/**/schema.graphql",
+  schema: path.resolve(__dirname, "**/schema.graphql"),
   generates: {
     "src/apps/graphql/schema": defineConfig(),
   },
