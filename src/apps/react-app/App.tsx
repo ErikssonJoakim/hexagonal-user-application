779d1cee3e7a1e1eb67fcf0c77223305e317610c
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-26 19:56:18 +0100

    feat(app): init react-app

---
+++ src/apps/react-app/App.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <div style={{ height: "inherit" }}>
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+export default App;
