41b65d59e542c9232784e6c5005f5c7e22bbceee
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-29 01:53:48 +0100

    feat(app): create user context in react-app

--- src/apps/react-app/App.tsx
+++ src/apps/react-app/App.tsx
@@ -1,11 +1,17 @@
 import type { FC } from 'react'
+import { useContext } from 'react'
 import { Router } from 'React-App/components/router/router'
+import { UserContext } from 'React-App/contexts/user.context'
 
 const App: FC = () => {
+  const context = useContext(UserContext)
+
   return (
     <div style={{ height: 'inherit' }}>
       <div className="page-layout">
-        <Router />
+        <UserContext.Provider value={context}>
+          <Router />
+        </UserContext.Provider>
       </div>
     </div>
   )
