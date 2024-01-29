c5288d4acd3f2ac2fc652e0a993397a83884a762
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-29 16:34:47 +0100

    feat(app): add react-app login form to home page

--- src/apps/react-app/pages/home/home.tsx
+++ src/apps/react-app/pages/home/home.tsx
@@ -1,19 +1,22 @@
 import type { FC } from 'react'
 import { useContext } from 'react'
 import { UserContext } from 'React-App/contexts/user.context'
+import { LoginForm } from 'React-App/views/home/loginForm/loginForm'
 
 export const Home: FC = () => {
   const { user } = useContext(UserContext)
-  
+
   return (
     <div className="react-app-home-page-main">
-      {user && (
+      {user ? (
         <>
           <p>
             Welcome {user.firstName}, {user.lastName}
           </p>
           <p>You logged in with email: {user.email}</p>
         </>
+      ) : (
+        <LoginForm />
       )}
     </div>
   )
