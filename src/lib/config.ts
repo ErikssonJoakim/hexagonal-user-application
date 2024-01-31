359c38d4b4dd3e024d7ebc3ad27f03f9a3a88a06
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-31 11:51:25 +0100

    build: allow production or development .env files

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,23 @@
-import 'dotenv/config'
+import { ResourceNotFoundError, presentResourceError } from '@/shared/errors/resource'
+import dotenv from 'dotenv'
 import type { Config } from '@/types/config.type'
+import fs from 'fs'
+
+enum EnvFile {
+  PRODUCTION = '.env.production',
+  DEVELOPMENT = '.env.development'
+}
+
+// Prioritizes using production environment before development
+const envFiles = fs
+  .readdirSync('.')
+  .filter(file => file === EnvFile.PRODUCTION || file === EnvFile.DEVELOPMENT)
+  .sort((a, b) => b.localeCompare(a))
+
+if (envFiles.length === 0)
+  throw presentResourceError(ResourceNotFoundError([EnvFile.PRODUCTION, EnvFile.DEVELOPMENT]))
+
+dotenv.config({ path: envFiles[0] })
 
 export const config: Config = {
   node: {
@@ -11,7 +29,7 @@ export const config: Config = {
       port: process.env.MYSQL_PORT,
       user: process.env.MYSQL_USER,
       database: process.env.MYSQL_DATABASE,
-      password: process.env.MYSQL_PASSWORD
+      password: process.env.MYSQL_ROOT_PASSWORD
     }
   }
 }
