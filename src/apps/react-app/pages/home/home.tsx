84077e66f9d3b6867a3174ecb1c21fcb34ae52b1
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-31 19:20:21 +0100

    feat(apps): add simple logout

--- src/apps/react-app/pages/home/home.tsx
+++ src/apps/react-app/pages/home/home.tsx
@@ -1,21 +1,27 @@
+import { Button } from '@/apps/design-system/ui/atoms/button/button'
 import type { FC } from 'react'
-import { useContext } from 'react'
+import { useCallback, useContext } from 'react'
 import { UserContext } from 'React-App/contexts/user.context'
 import { LoginForm } from 'React-App/views/home/loginForm/loginForm'
 import './home.scss'
 
 export const Home: FC = () => {
-  const { user } = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext)
+
+  const handleLogout = useCallback(() => {
+    setUser(null)
+  }, [setUser])
 
   return (
     <div className="react-app-home-page-main">
       {user ? (
-        <>
+        <div className="react-app-home-page-logout-welcome">
           <p>
             Welcome {user.firstName}, {user.lastName}
           </p>
           <p>You logged in with email: {user.email}</p>
-        </>
+          <Button label="Logout" onClick={handleLogout} />
+        </div>
       ) : (
         <LoginForm />
       )}
