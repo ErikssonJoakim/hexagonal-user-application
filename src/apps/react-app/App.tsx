9189b87bd99d6517e439cf3d59a73519bb3e3cb5
ErikssonJoakim <erikssonjoakim92@gmail.com>
2024-01-31 18:29:34 +0100

    feat(apps): rework react-app user context

--- src/apps/react-app/App.tsx
+++ src/apps/react-app/App.tsx
@@ -1,10 +1,44 @@
 import type { FC } from 'react'
-import { useContext } from 'react'
+import { useMemo } from 'react'
 import { Router } from 'React-App/components/router/router'
 import { UserContext } from 'React-App/contexts/user.context'
+import { RegisterUserUseCase } from '@/application/usecases/register-user.usecase'
+import { RealIdProvider } from '@/infra/real-id-provider'
+import { RealDateProvider } from '@/infra/real-date-provider'
+import { ApolloUserRepositoryAdaptor } from '@/infra/apollo/user.apollo.api.adapter'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const App: FC = () => {
-  const context = useContext(UserContext)
+  const cache = useMemo(() => new InMemoryCache(), [])
+
+  const client = useMemo(
+    () =>
+      new ApolloClient({
+        cache,
+        uri: `http://localhost:4000/`,
+
+        name: 'react-app-client',
+        defaultOptions: {
+          watchQuery: {
+            fetchPolicy: 'cache-and-network'
+          }
+        }
+      }),
+    [cache]
+  )
+
+  const realIdProvider = useMemo(() => new RealIdProvider(), [])
+  const realDateProvider = useMemo(() => new RealDateProvider(), [])
+  const apolloAPI = useMemo(() => new ApolloUserRepositoryAdaptor(client), [client])
+  const registerUserUseCase = useMemo(
+    () => new RegisterUserUseCase(apolloAPI, realIdProvider, realDateProvider),
+    [apolloAPI, realDateProvider, realIdProvider]
+  )
+
+  const context = {
+    register: registerUserUseCase.handle.bind(registerUserUseCase),
+    getByID: apolloAPI.getByID.bind(apolloAPI)
+  }
 
   return (
     <div style={{ height: 'inherit' }}>
--- src/apps/react-app/contexts/user.context.ts
+++ src/apps/react-app/contexts/user.context.ts
@@ -1,34 +1,10 @@
 import { createContext } from 'react'
 import type { RegisterUserCommand } from '@/application/usecases/register-user.usecase'
-import { RegisterUserUseCase } from '@/application/usecases/register-user.usecase'
-import { RealIdProvider } from '@/infra/real-id-provider'
-import { RealDateProvider } from '@/infra/real-date-provider'
-import { ApolloUserRepositoryAdaptor } from '@/infra/apollo/user.apollo.api.adapter'
-import { ApolloClient, InMemoryCache } from '@apollo/client'
 import type { ID } from '@/types/super-types'
 import type { ResourceAlreadyExistsError, ResourceNotFoundError } from '@/shared/errors/resource'
 import type { NetworkError } from '@/shared/errors/network'
 import type { SerializationError } from '@/shared/errors/serialization'
 import type { User } from '@/domain/user'
-
-const cache = new InMemoryCache()
-const client = new ApolloClient({
-  cache,
-  uri: 'http://localhost:4000/',
-
-  name: 'react-app-client',
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'cache-and-network'
-    }
-  }
-})
-
-const realIdProvider = new RealIdProvider()
-const realDateProvider = new RealDateProvider()
-const apolloAPI = new ApolloUserRepositoryAdaptor(client)
-
-const registerUserUseCase = new RegisterUserUseCase(apolloAPI, realIdProvider, realDateProvider)
 
 type UserContext = {
   register: (
@@ -39,6 +15,13 @@ type UserContext = {
 }
 
 export const UserContext = createContext<UserContext>({
-  register: registerUserUseCase.handle.bind(registerUserUseCase),
-  getByID: apolloAPI.getByID.bind(apolloAPI)
+  register: async () =>
+    new Promise((_, reject) => {
+      reject('register not set')
+    }),
+  getByID: async () =>
+    new Promise((_, reject) => {
+      reject('getByID not set')
+    }),
+  user: null
 })
